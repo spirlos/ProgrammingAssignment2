81e9e858994cee37ce432005263cd741068d9657
spyros <sp.pilos@gmail.com>
2016-02-29 10:30:15 +0100

    Cache matrix assignment
    Coursera R programming assignment for week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
 ## The purpose of cacheMatrix is to avoid computing again the inverse of a Matrix, 
 ## which is costly in terms of resource, if the caluckation has already been done.
-## inversion, which is usually a costly computation. This is done with caching, which  
-## uses two funcions, one for creating the cache for the inversed matrix and
+## This is done with caching, which  uses two funcions: 
+## one for creating the cache for the inversed matrix and
 ## one which fills the cache matrix the first time it runs and calculates the inverse
-## and skips the calculation afterwards.
+## and then skips the calculation afterwards.
 ##
 ## makeCacheMatrix creates a list of functions which:
 ## - set the value for the matrix
@@ -22,7 +22,7 @@ makeCacheMatrix <- function(x = matrix()) {
         getinverse <- function() i
         list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-## cacheSolven returns the inverse of the matrix. If the inverse has not been computed yet
+## cacheSolve returns the inverse of the matrix. If the inverse has not been computed yet
 ## (first time it is run) it computes it (through solve()), and fills the cache (through setinverse()) 
 ## If it has already been computed, it skips the calcuation and give the cached result 
 ## with the message "getting cached data" 
